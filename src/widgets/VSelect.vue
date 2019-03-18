15ac4961a51f5536a970ec4bfe095c88d59a6150
liwei <m15756296407@163.com>
2019-03-18 10:23:51 +0800

    111

--- src/widgets/VSelect.vue
+++ src/widgets/VSelect.vue
@@ -12,12 +12,25 @@
           :value="item.value"
         ></el-option>
       </el-select>
+      <div v-for="model in ooptions">
+        <div>
+          <i v-if="isFolder" class="fa"></i>
+          <!--isFolder判断是否存在子级改变图标-->
+          <i v-if="!isFolder" class="fa fa-file-text"></i>
+          {{model.label}}
+        </div>
+        <ul v-if="isFolder">
+          <items v-for="cel in model.children" :model="cel"></items>
+        </ul>
+      </div>
     </el-col>
   </el-row>
 </template>
 <script>
 import axios from 'axios';
+let cfg = { "status": "SUCCESS", "data": [{ "id": "1", "parentId": "-1", "label": "数据源", "children": [{ "id": "2", "parentId": "1", "label": "关系型数据库数据源", "children": null }] }, { "id": "3", "parentId": "-1", "label": "数据源1", "children": [{ "id": "4", "parentId": "3", "label": "关系型数据库数据源2", "children": null }] }, { "id": "5", "parentId": "-1", "label": "机器学习", "children": [{ "id": "6", "parentId": "5", "label": "分类", "children": [{ "id": "9", "parentId": "6", "label": "二分类", "children": null }, { "id": "10", "parentId": "6", "label": "多分类", "children": null }] }, { "id": "7", "parentId": "5", "label": "回归", "children": null }, { "id": "8", "parentId": "5", "label": "聚类", "children": null }, { "id": "9", "parentId": "6", "label": "二分类", "children": null }, { "id": "10", "parentId": "6", "label": "多分类", "children": null }] }], "message": null };
 export default {
+  name: 'items',
   props: {
     data: {
       type: Object
@@ -27,6 +40,9 @@ export default {
     },
     value: {
       type: String
+    },
+    model: {
+      type: Object
     }
   },
   data () {
@@ -46,12 +62,19 @@ export default {
       }, {
         value: '选项5',
         label: '北京烤鸭'
-      }]
+      }],
+      ooptions: cfg,
+      value7: ''
     }
   },
   methods: {
     onChange (value) {
       this.ok(value)
+    }
+  },
+  computed: {
+    isFolder () {
+      return this.model.children && this.model.children.length
     }
   },
   mounted () {
